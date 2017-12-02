ed54e70b3f25e60b50b9e895174fdd33b5c422b3
Sijmen Schoon <sijmenschoon@gmail.com>
2017-12-02 23:03:08 +0100

    Simplify a bit

--- day2.hs
+++ day2.hs
@@ -1,13 +1,13 @@
 module Main where
 
-day2A :: [[Int]] -> Int
-day2A input = sum $ map (\x -> maximum x - minimum x) input
+day2A :: [Int] -> Int
+day2A x = maximum x - minimum x
 
-day2B :: [[Int]] -> Int
-day2B input = sum $ map (\x -> head [quot a b | a <- x, b <- x, a > b && a `rem` b == 0]) input
+day2B :: [Int] -> Int
+day2B x = head [quot a b | a <- x, b <- x, a > b && a `rem` b == 0]
 
 main :: IO ()
 main = do
   input <- map (map read . words) . lines <$> getContents
-  print $ day2A input
-  print $ day2B input
+  print $ sum $ map day2A input
+  print $ sum $ map day2B input
