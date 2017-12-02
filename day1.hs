dcb3581e3e769c4bfd15f9756e0643843b54c506
Sijmen Schoon <sijmenschoon@gmail.com>
2017-12-03 00:32:42 +0100

    Make day 1 semireadable

--- day1.hs
+++ day1.hs
@@ -2,22 +2,19 @@ module Main where
 
 import Data.Char (digitToInt)
 
-day1A :: Char -> String -> Int
-day1A first (a:b:t) = day1A first (b:t) + day1A a [b]
-day1A first [h]     = if h == first then digitToInt h else 0
-day1A _ []          = 0
+day1A :: [Int] -> Int
+day1A (a:b:t) = day1A (b:t) + if a == b then a else 0
+day1A _       = 0
 
-day1B :: String -> String -> Int
-day1B (headOrig:tailOrig) (headShift:tailShift) =
-  day1B tailOrig tailShift + if headOrig == headShift then digitToInt headOrig else 0
-day1B _ _ = 0
+day1B :: [Int] -> [Int] -> Int
+day1B (a:at) (b:bt) = day1B at bt + if a == b then a else 0
+day1B _ _           = 0
 
-rotate :: [Char] -> [Char]
-rotate input = zipWith const (drop halfLength $ cycle input) input
-  where halfLength = length input `quot` 2
+rotate :: [Int] -> [Int]
+rotate x = drop (length x `quot` 2) $ cycle x
 
 main :: IO ()
 main = do
-  input <- getLine
-  print $ day1A (head input) input
+  input <- map digitToInt <$> getLine
+  print $ day1A (head input:input)
   print $ day1B input $ rotate input
