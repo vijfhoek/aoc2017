40b6cad7b1b66dd1380a4b0d9a8b5cb9de9dc28f
Sijmen Schoon <sijmenschoon@gmail.com>
2017-12-03 23:24:10 +0100

    Clean up

--- day3.hs
+++ day3.hs
@@ -2,8 +2,8 @@ module Main where
 import Data.Maybe (mapMaybe)
 import qualified Data.Map.Lazy as M
 
-coords :: Int -> (Int, Int) -> (Int, Int)
-coords steps (x, y) = case (steps - 1) `rem` 4 of
+step :: Int -> (Int, Int) -> (Int, Int)
+step steps (x, y) = case (steps - 1) `rem` 4 of
   0 -> (x + 1, y)
   1 -> (x, y + 1)
   2 -> (x - 1, y)
@@ -20,16 +20,17 @@ calcValue grid (x, y) =
 part1 :: Int -> Int -> Int -> (Int, Int) -> Int
 part1 n steps (-1) c = part1 n (steps + 1) (steps `quot` 2) c
 part1 n steps i (x, y)
-  | n > 0     = part1 (n - 1) steps (i - 1) (coords steps (x, y))
+  | n > 0     = part1 (n - 1) steps (i - 1) (step steps (x, y))
   | otherwise = abs x + abs y
 
-part2 :: Int -> (M.Map (Int, Int) Int) -> Int -> Int -> (Int, Int) -> Int
-part2 n grid steps (-1) c = part2 n grid (steps +1) (steps `quot` 2) c
-part2 n grid steps i (x, y)
-  | value < n = part2 n (M.insert (nx, ny) value grid) steps (i - 1) (nx, ny)
+part2 :: (M.Map (Int, Int) Int) -> Int -> Int -> Int -> (Int, Int) -> Int
+part2 grid n steps (-1) coords = part2 grid n (steps + 1) (steps `quot` 2) coords
+part2 grid n steps i coords
+  | value < n = part2 nextGrid n steps (i - 1) next
   | otherwise = value
-  where (nx, ny) = coords steps (x, y)
-        value = calcValue grid (nx, ny)
+  where next = step steps coords
+        nextGrid = M.insert next value grid
+        value = calcValue grid next
 
 main :: IO ()
 main = do
@@ -37,4 +38,4 @@ main = do
   print $ part1 (n - 1) 0 (-1) (0, 0)
 
   let grid = M.singleton (0, 0) 1
-  print $ part2 n grid 0 (-1) (0, 0)
+  print $ part2 grid n 0 (-1) (0, 0)
