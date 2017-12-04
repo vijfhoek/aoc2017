35d15287875f9865819bae60283c3bb5e65c134a
Sijmen Schoon <sijmenschoon@gmail.com>
2017-12-04 19:11:26 +0100

    Didn't need sets after all

--- day4.hs
+++ day4.hs
@@ -1,17 +1,14 @@
 module Main where
 import Data.List
-import qualified Data.Set as S
 
-part1 :: String -> Bool
-part1 pp = length set == length (words pp)
-  where set = foldl (flip S.insert) S.empty (words pp)
+part1 :: [String] -> Bool
+part1 pp = length pp == length (nub pp)
 
-part2 :: String -> Bool
-part2 pp = length set == length (words pp)
-  where set = foldl (\acc pw -> S.insert (sort pw) acc) S.empty (words pp)
+part2 :: [String] -> Bool
+part2 pp = length pp == length (nub $ map sort pp)
 
 main :: IO ()
 main = do
   input <- lines <$> getContents
-  print $ (length . filter part1) input
-  print $ (length . filter part2) input
+  print $ (length . filter part1) $ map words input
+  print $ (length . filter part2) $ map words input
