17f1607ac075ba9e3baffaebc82e19d618915e38
Sijmen Schoon <sijmenschoon@gmail.com>
2017-12-05 13:35:39 +0100

    Remove unnecessary brackets

--- day3.hs
+++ day3.hs
@@ -9,7 +9,7 @@ step steps (x, y) = case (steps - 1) `rem` 4 of
   2 -> (x - 1, y)
   _ -> (x, y - 1)
 
-calcValue :: (M.Map (Int, Int) Int) -> (Int, Int) -> Int
+calcValue :: M.Map (Int, Int) Int -> (Int, Int) -> Int
 calcValue grid (x, y) =
   sum $ mapMaybe (grid M.!?)
     [ (x - 1, y - 1), (x, y - 1), (x + 1, y - 1)
@@ -23,7 +23,7 @@ part1 n steps i (x, y)
   | n > 0     = part1 (n - 1) steps (i - 1) (step steps (x, y))
   | otherwise = abs x + abs y
 
-part2 :: (M.Map (Int, Int) Int) -> Int -> Int -> Int -> (Int, Int) -> Int
+part2 :: M.Map (Int, Int) Int -> Int -> Int -> Int -> (Int, Int) -> Int
 part2 grid n steps (-1) coords = part2 grid n (steps + 1) (steps `quot` 2) coords
 part2 grid n steps i coords
   | value < n = part2 nextGrid n steps (i - 1) next
